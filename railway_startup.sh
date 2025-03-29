fa849a2509cd646aadce2d6e377b5f9138d8e55e
truss255 <tanrrussell@gmail.com>
2025-03-29 13:15:13 -0400

    commit and push to main

--- railway_startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-gunicorn main:app \
-    --workers $GUNICORN_WORKERS \
-    --threads $GUNICORN_THREADS \
-    --timeout $GUNICORN_TIMEOUT \
-    --bind 0.0.0.0:$PORT \
-    --log-level info \
-    --access-logfile - \
-    --error-logfile -
